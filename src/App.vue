bb299b9adb5dedca6165b7e2d7c9a60c695d1d6e
Nisitha Sandanayake <nisithanuwanga@gmail.com>
2026-04-27 21:09:02 +0530

    Create home.vue

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,8 @@
-<template></template>
+<template>
+  <h1 class="text-4xl font-bold text-green-500">
+    Tailwind working 🚀
+  </h1>
+
+  <router-view />
+
+</template>
---
+++ src/router/index.ts
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory, type RouteRecordRaw} from 'vue-router'
+
+import Home from '../views/Home.vue'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export { router}
