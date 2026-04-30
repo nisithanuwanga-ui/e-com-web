fa572fdfc3e67b318b98317b958c5c6d8aeeb54f
Nisitha Sandanayake <nisithanuwanga@gmail.com>
2026-04-30 12:49:15 +0530

    Register Cart route and update main navigation

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,14 @@
 import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
 import ProductView from '../views/ProductView.vue'
-import LoginView from '../views/LoginView.vue' // 1. Import it
+import LoginView from '../views/LoginView.vue' 
+import CartView from '../views/CartView.vue'
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/product/:id',
-    name: 'ProductDetail',
-    component: ProductView,
-  },
-  {
-    path: '/login', // 2. Add the route
-    name: 'Login',
-    component: LoginView,
-  }
+  { path: '/', name: 'Home', component: Home,},
+  { path: '/product/:id', name: 'ProductDetail', component: ProductView,},
+  { path: '/login', name: 'Login', component: LoginView,},
+  { path: '/cart', name: 'Cart', component: CartView }
 ]
 
 const router = createRouter({
